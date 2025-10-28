7a7e889549065423c10874de37ccfd06467b5405
Lucy <luluhop24@gmail.com>
2025-10-28 10:45:11 -0400

    Deleted Comments, created a second class for the remote. also made an arm class

--- src/Remote2.java
+++ src/Remote2.java
@@ -1,24 +1,14 @@
 import java.util.Scanner;
+public class Remote2 {
 
-public class Remote {
-
-    // Simulated servo position (0.0 = rest, 1.0 = flipped)
-    private static double servoPosition = 0.0;
-
-    // Flip speed (milliseconds between motion updates)
-    static final int FLIP_DELAY = 1000;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("📖 Page Flipper Robot Simulation");
-        System.out.println("Press ENTER to flip a page or type 'exit' to quit.");
-
         while (true) {
-            System.out.print("\nCommand: ");
             String input = scanner.nextLine();
 
             if (input.equalsIgnoreCase("exit")) {
-                System.out.println("👋 Shutting down. Goodbye!");
+
                 break;
             }
 
@@ -30,17 +20,17 @@ public class Remote {
     private static void flipPage() {
 
         moveServo(1.0);
-        waitMs(FLIP_DELAY);
+        waitMs(Remote.FLIP_DELAY);
 
 
         moveServo(0.0);
-        waitMs(FLIP_DELAY);
+        waitMs(Remote.FLIP_DELAY);
 
 
     }
 
     private static void moveServo(double position) {
-        servoPosition = position;
+        double servoPosition = position;
 
     }
 
@@ -52,3 +42,8 @@ public class Remote {
         }
     }
 }
+
+
+
+
+
